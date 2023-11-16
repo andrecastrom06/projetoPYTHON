880db3dc08ae0108aaf719b9bce573b073657ab0
Fcc2187 <142419397+Fcc2187@users.noreply.github.com>
2023-11-16 19:56:38 -0300

    Update index.py

--- index.py
+++ index.py
@@ -16,34 +16,34 @@ def main():
         print("0 - Parar o programa")
         opção = int(input())
         if opção == 1:
-            nome = input("Digite o nome do livro: ")
-            autor = input("Digite o nome do autor do livro: ")
+            nome = input("Digite o nome do livro: ").capitalize()
+            autor = input("Digite o nome do autor do livro: ").capitalize()
             try:
                 preço = float(input("Digite o preço do livro: "))
             except ValueError:
                 print("\nERRO!")
                 print("Preço tem que ser um número.")
                 continue
-            genero = input("Digite o genero do livro: ")
+            genero = input("Digite o genero do livro: ").capitalize()
             functions.adicionarLivro("books.txt", nome, autor, str(preço), genero)
         elif opção == 2:
-            nome = input("Digite o nome do livro que você deseja remover: ")
+            nome = input("Digite o nome do livro que você deseja remover: ").capitalize()
             functions.removerLivro("books.txt", nome)
         elif opção == 3:
             functions.agruparLivros("books.txt")
         elif opção == 4:
             print("Gastos totais:", functions.gastosTotais("books.txt"), "R$")
         elif opção == 5:
-            genero = input("Digite o genero que você deseja buscar:")
+            genero = input("Digite o genero que você deseja buscar:").capitalize()
             functions.buscar("books.txt", genero)
         elif opção == 6:
             functions.verBiblioteca("books.txt")
         elif opção == 7:
             functions.livroMaisCaro("books.txt")
         elif opção == 8:
-            nome = input("Digite o nome do livro que você deseja atualizar: ")
+            nome = input("Digite o nome do livro que você deseja atualizar: ").capitalize()
             opcao = input("Digite o que você deseja atualizar no livro ('nome', 'preço', 'autor' ou 'genero'): ")
-            valor = input(f"Digite o novo {opcao} que ira substituir o antigo: ")
+            valor = input(f"Digite o novo {opcao} que ira substituir o antigo: ").capitalize()
             functions.atualizarLivro("books.txt", nome, opcao, valor)
         elif opção == 0:
             print("Desligando...")
